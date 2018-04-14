43299a7137b9477520ba0bd76310ff7aa82ba435
Anastassios Martakos <anastassios.martakos@gmail.com>
2018-04-14 20:03:02 +0200

    Remove unused variables

--- src/graph/symbol.hpp
+++ src/graph/symbol.hpp
@@ -7,12 +7,11 @@ namespace realmar::turing {
     struct symbol final {
     private:
         std::shared_ptr<T> _symbol = nullptr;
-        T* raw;
 
     public:
         symbol() = default;
 
-        explicit symbol(const T& symbol) : _symbol(std::make_shared<T>(symbol)) { raw = _symbol.get(); }
+        explicit symbol(const T& symbol) : _symbol(std::make_shared<T>(symbol)) {}
 
         const std::shared_ptr<T>& get_symbol() const {
             return _symbol;
@@ -20,13 +19,9 @@ namespace realmar::turing {
 
         void set_symbol(std::shared_ptr<T> symbol) {
             _symbol = symbol;
-            raw = _symbol.get();
         }
 
         bool operator==(const symbol<T>& other) const {
-            auto s1 = get_symbol();
-            auto s2 = other.get_symbol();
-
             if (get_symbol() == nullptr && other.get_symbol() == nullptr) {
                 return true;
             } else if (get_symbol() == nullptr || other.get_symbol() == nullptr) {
